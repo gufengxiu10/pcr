c73199b99f3477759e90c3d0466fdcb7ac5ae3c5
anng <296718933@qq.com>
2021-01-25 23:53:40 +0800

    1

--- project/php/swoole/test.php
+++ project/php/swoole/test.php
@@ -1,11 +1,8 @@
 <?php
 
-use Anng\Plug\Oos\Aliyun\Objects;
-use Anng\Plug\Oos\Auth;
 use GuzzleHttp\Client;
-use OSS\Model\BucketInfo;
 use Predis\Client as PredisClient;
-use Swoole\Process;
+use Swlib\SaberGM;
 
 require_once "vendor/autoload.php";
 
@@ -13,8 +10,9 @@ date_default_timezone_set("Asia/Shanghai");
 
 Swoole\Runtime::enableCoroutine();
 $s = microtime(true);
-Co\run(
+\Co\run(
     function () {
+
         $redis = new PredisClient([
             'host' => '172.200.1.7',
             'prot'  => 6379
@@ -22,12 +20,12 @@ Co\run(
 
         $redis->auth('gufengxiu10');
 
-        $date = '2021-01-06';
+        $date = '2021-01-23';
         $cliden = new Client;
         if (!$redis->exists('pxixv-' . $date)) {
             $d = [];
             for ($i = 1; $i < 4; $i++) {
-                $res = $cliden->request('get', 'https://pixiviz.pwp.app/api/v1/illust/rank?mode=day&date=' . $date . '&page=' . $i, [
+                $res = SaberGM::get('https://pixiviz.pwp.app/api/v1/illust/rank?mode=day&date=' . $date . '&page=' . $i, [
                     'heards' => [
                         'User-Agent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.96 Safari/537.36 Edg/88.0.705.50'
                     ]
@@ -52,45 +50,55 @@ Co\run(
         dump($fd);
         // https://pixiv-image-jp.pwp.link/img-original/img/2021/01/07/00/00/08/86871399_p0.jpg
         foreach ($fd as $key => $val) {
-            go(function () use ($cliden, $key, $val, $date, &$ki) {
-                Swoole\Coroutine\System::sleep(0.5);
+            go(function () use ($key, $val, $date, &$ki) {
                 foreach ($val as $k => $v) {
-                    $res = $cliden->request('GET', str_replace('https://i.pximg.net', 'https://pixiv-image-jp.pwp.link', $v), [
-                        'headers' => [
-                            'user-agent' => 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:30.0) Gecko/20100101 Firefox/30.0',
-                            ':authority' => 'pixiv-image-jp.pwp.link',
-                            ':method' => 'GET',
-                            ':path' => str_replace('https://i.pximg.net', '', $v),
-                            ':scheme' => 'https',
-                            'origin' => 'https://pixiviz.pwp.app',
-                            'referer' => 'https://pixiviz.pwp.app/',
-                        ]
-                    ]);
+                    try {
+                        $res = SaberGM::get(str_replace('https://i.pximg.net', 'https://pixiv-image-jp.pwp.link', $v), [
+                            'headers' => [
+                                'user-agent' => 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:30.0) Gecko/20100101 Firefox/30.0',
+                                ':authority' => 'pixiv-image-jp.pwp.link',
+                                ':method' => 'GET',
+                                ':path' => str_replace('https://i.pximg.net', '', $v),
+                                ':scheme' => 'https',
+                                'origin' => 'https://pixiviz.pwp.app',
+                                'referer' => 'https://pixiviz.pwp.app/',
+                            ],
+                            'timeout' => 20,
+                        ]);
 
-                    if ($res->getStatusCode() != 200) {
-                        dump($res->getStatusCode());
-                        continue;
-                    }
-                    $body = $res->getBody();
-                    $fileName = substr($v, strrpos($v, '/') + 1);
-                    if (!is_dir('./im/' . $date)) {
-                        mkdir('./im/' . $date, 0777, true);
-                    }
-
-                    go(function () use ($date, $fileName, $body, $key, &$ki, $k) {
-                        $al = './im/' . $date . '/' . $fileName;
-                        $wd = fopen($al, 'w');
-
-                        while (!$body->eof()) {
-                            $content = $body->read(1024 * 200);
-                            fwrite($wd, $content);
+                        if ($res->getStatusCode() != 200) {
+                            dump($res->getStatusCode());
+                            continue;
                         }
 
-                        fclose($wd);
-                        dump('成功：' . $key . '-' . $k . '-' . $fileName);
-                        unset($ki[$key][$k]);
-                        dump($ki);
-                    });
+                        $body = $res->getBody();
+                        $fileName = substr($v, strrpos($v, '/') + 1);
+                        if (!is_dir('./im/' . $date)) {
+                            mkdir('./im/' . $date, 0777, true);
+                        }
+
+                        if( file_exists('./im/' . $date . '/' . $fileName) ){
+                            unset($ki[$key][$k]);
+                            continue;
+                        }
+
+                        go(function () use ($date, $fileName, $body, $key, $k, &$ki) {
+                            $al = './im/' . $date . '/' . $fileName;
+                            $wd = fopen($al, 'w');
+
+                            while (!$body->eof()) {
+                                $content = $body->read(1024 * 200);
+                                fwrite($wd, $content);
+                            }
+
+                            fclose($wd);
+                            dump('成功：' . $key . '-' . $k . '-' . $fileName);
+                            unset($ki[$key][$k]);
+                            dump($ki);
+                        });
+                    } catch (\Throwable $th) {
+                        continue;
+                    }
                 }
             });
         }
