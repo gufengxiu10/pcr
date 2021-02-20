4f26f95a9fa5bd17e2418b105b1d86c34bf57c60
anng <296718933@qq.com>
2021-02-20 09:42:43 +0800

    123

---
+++ project/php/swoole/app/Api/music/contract/Api.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Api\music\contract;
+
+interface Api
+{
+    /**
+     * @name: 搜索
+     * @param {*}
+     * @author: ANNG
+     * @todo: 
+     * @Date: 2021-02-19 09:56:55
+     * @return {*}
+     */
+    public function search();
+}
---
+++ project/php/swoole/app/Api/music/son/We.php
@@ -0,0 +1,18 @@
+<?php
+
+use App\Api\music\contract\Api;
+
+class We implements Api
+{
+
+    public function search()
+    {
+        # code...
+        // new Exception()
+    }
+
+    public function info()
+    {
+        # code...
+    }
+}
