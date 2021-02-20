72307b5fe27d01af81ddeecbcd2023902c621f1a
anng <296718933@qq.com>
2021-02-20 17:52:40 +0800

    123

--- project/php/swoole/app/api/music/Base.php
+++ project/php/swoole/app/api/music/Base.php
@@ -2,19 +2,10 @@
 
 declare(strict_types=1);
 
-namespace app\api\music;
-
-use app\api\music\son\Netease;
-
 class Base
 {
-
-    public function test($key)
+    public function set()
     {
-        $netease = new Netease();
-        // return $netease->search('寒蝉');
-        // return $netease->song('672188');
-        // return $netease->album('77700');
-        return $netease->setDownLoad(true)->searchOnce($key);
+        # code...
     }
 }
